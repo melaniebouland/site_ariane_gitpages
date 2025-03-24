16c97d01ded2fc38b655fe0fe0877a8968895308
Mélanie Bouland <130862529+melatypical@users.noreply.github.com>
2025-03-24 13:53:11 +0100

    commit 24/03
    Ajout des logos sans textes

--- generate_json.py
+++ generate_json.py
@@ -1,8 +1,5 @@
 import os
 import json
-import time
-from watchdog.observers import Observer
-from watchdog.events import FileSystemEventHandler
 from generate_html import generate_html_pages
 
 # Dossier contenant les documents
@@ -28,23 +25,5 @@ def generate_json(directory, output_file):
     print(f"Fichier JSON mis à jour : {output_file}")
     generate_html_pages(output_file, OUTPUT_DIR)
 
-class TravauxHandler(FileSystemEventHandler):
-    def on_any_event(self, event):
-        if event.event_type in ["created", "deleted", "modified", "moved"]:
-            print("Changement détecté, mise à jour en cours...")
-            generate_json(TRAVAUX_DIR, OUTPUT_JSON)
-
 if __name__ == "__main__":
-    event_handler = TravauxHandler()
-    observer = Observer()
-    observer.schedule(event_handler, TRAVAUX_DIR, recursive=True)
-    observer.start()
-    
-    try:
-        print("Surveillance en cours... (Ctrl+C pour arrêter)")
-        while True:
-            time.sleep(10)
-    except KeyboardInterrupt:
-        observer.stop()
-        print("Arrêt de la surveillance.")
-    observer.join()
+    generate_json(TRAVAUX_DIR, OUTPUT_JSON)
